3b31efe1f940c8eaaf7528711d33b062de789b77
the6thm0nth <the6thm0nth@outlook.com>
2018-07-04 12:35:13 +0900

    Set: remove redux-thunk config

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -16,8 +16,6 @@ function configureStoreProd(initialState) {
   const middlewares = [
     // Add other middleware on this line...
 
-    // thunk middleware can also accept an extra argument to be passed to each thunk action
-    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
     sagaMiddleware,
     reactRouterMiddleware
   ];
@@ -41,8 +39,6 @@ function configureStoreDev(initialState) {
     // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
     reduxImmutableStateInvariant(),
 
-    // thunk middleware can also accept an extra argument to be passed to each thunk action
-    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
     sagaMiddleware,
     reactRouterMiddleware
   ];
